ec0c27585f93df93d36c22b27b7bd978b5ae30f6
Omar-Ashraf-Mosaad <109628423+Omar-Ashraf-Mosaad@users.noreply.github.com>
2025-11-08 16:43:46 +0200

    Structure fixes
    changed Buymate.Models to BuyMate.Model
    
    moved ViewModels to BuyMate.DTO

---
+++ BuyMate.BLL/Features/User/Register/IRegisterUserHandler.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuyMate.BLL.Features.User.Register
+{
+    internal interface IRegisterUserHandler
+    {
+
+    }
+}
--- BuyMate/Models/ErrorViewModel.cs
+++ BuyMate/Models/ErrorViewModel.cs
@@ -1,4 +1,4 @@
-namespace BuyMate.Models
+namespace BuyMate.Model
 {
     public class ErrorViewModel
     {

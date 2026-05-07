a8377d31065068803746f4fee6f6ffcda3ef97f2
Omar <narutoomar12@gmail.com>
2026-05-07 13:52:53 +0300

    increased image size

--- BuyMate.BLL/Constants/AppConstants.cs
+++ BuyMate.BLL/Constants/AppConstants.cs
@@ -3,7 +3,7 @@
     public static class AppConstants
     {
         public static readonly string[] AllowedImageExtensions = new[] { ".jpg", ".jpeg", ".png", ".gif", ".webp" };
-        public const long MaxImageFileSizeBytes = 2 * 1024 * 1024; // 2 MB
+        public const long MaxImageFileSizeBytes = 20 * 1024 * 1024; // 20 MB
         public const string DefaultProfileImageFileName = "Default.webp";
         public const string UserProfileImagesFolder = "UserProfileImages";
         public const string CategoriesFolderName = "Categories";
